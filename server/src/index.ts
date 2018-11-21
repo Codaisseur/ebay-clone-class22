75e36fbb88177313e05999ad6b2ec1981f1b7757
Arien Kock <arien@positor.nl>
2018-11-21 15:27:31 +0100

    Empty API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world")
+import 'reflect-metadata'
+import {createKoaServer} from "routing-controllers"
+import Controller from "./controller"
+
+const port = process.env.PORT || 4000
+
+const app = createKoaServer({
+   controllers: [Controller]
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
